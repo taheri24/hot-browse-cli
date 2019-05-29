cf399cb8b3e3be9bb9ed6b46addb279dd512d8b5
Majid Taheri(I love optimization) <taheri24@users.noreply.github.com>
2019-05-29 13:26:10 +0430

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -11,18 +11,20 @@ class HotBrowseCli extends Command {
     help: flags.help({ char: 'h' }),
     url: flags.string({ required: true, char: 'u', description: 'URL for browse' }),
     watch: flags.string({ required: true, char: 'w', description: 'watching Directory or File for Hot Module Reload' }),
-  } 
+  }
 
   static args = [{ name: 'file' }]
-   
+
   async run() {
     const { args, flags } = this.parse(HotBrowseCli)
+    const watcher = watch(flags.watch || '', { persistent: true, recursive: true });
+
     const browser = await puppeteer.launch({ headless: false });
+    browser.on('disconnected', () => watcher.close());
     const page = await browser.newPage();
     await page.setBypassCSP(false);
     await page.setCacheEnabled(false);
     await page.goto(flags.url || '');
-    const watcher = watch(flags.watch || '', { persistent: true, recursive: true });
     watcher.addListener('change', async () => {
       const sessionData = await page.evaluate(function () {
         const { captureSessionData } = window as any;
@@ -36,7 +38,7 @@ class HotBrowseCli extends Command {
           loadCaptureSessionData(sessionData);
       }, sessionData);
     });
-  
+
   }
 }
 
